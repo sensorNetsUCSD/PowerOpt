1f91c314f56ffff6f4fc457c1da31ca8e8c285da
Larry <sensorNetsUCSD@gmail.com>
2013-05-15 19:26:11 -0700

    Updating and cleaning out other project

--- MEGA_UART_INTERRUPT_EXAMPLE1/MEGA_GPIO_EXAMPLE1/Debug/makedep.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit or delete the file
-################################################################################
-
-src\ASF\mega\boards\stk600\rcx_x\init.c
-
-src\mega_gpio_example.c
-
